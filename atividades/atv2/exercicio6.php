8747acdcc44fd0454eda6b92300316c03da066af
MarcosBrambilla <marcos.brambilla2003@gmail.com>
2026-02-26 21:14:49 -0300

    Atvidade2 Exercicios 1-6

---
+++ atividades/atv2/exercicio6.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Exercicio 6</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <div class="container py-3">
+      <h1>Exercicio 6</h1>
+      <form method="post">
+        <div class="mb-3">
+          <label for="nota1" class="form-label">Digite a temperatura em Graus Celsius: </label>
+          <input
+            type="number"
+            id="tempGraus"
+            name="tempGraus"
+            class="form-control"
+            required=""
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </form>
+      <?php
+      if ($_SERVER['REQUEST_METHOD'] == "POST") {
+          $c = $_POST['tempGraus'];
+
+          $f = ($c * 1.8) + 32;
+
+          echo("$c º Celsius é equivalente a $f º fahreheint");
+      }
+      ?>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+        crossorigin="anonymous"
+      ></script>
+    </div>
+  </body>
+</html>
