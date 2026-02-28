2920c7d115512096e9f6991449340bc4aa344108
Marcos Junior Dias Brambilla <marcos.brambilla2003@gmail.com>
2026-02-28 11:43:08 -0300

    Atividade 1 - criado script para navegar entre os forms

--- atividades/atv1/scripts.js
+++ atividades/atv1/scripts.js
@@ -2,12 +2,18 @@
 const buttons = document.querySelectorAll("#navbar button");
 const formContainer = document.getElementById("formContainer");
 
+const renderFormElement = async (formNumber) => {
+  const arquivo = `form${formNumber}.html`;
+  const response = await fetch(`forms/${arquivo}`);
+  const html = await response.text();
+  formContainer.innerHTML = html;
+}
+
 buttons.forEach(button => {
   button.addEventListener("click", async (b) => {
     const formNumber = b.target.innerText.split(" ")[1];
-    const arquivo = `form${formNumber}.html`;
-    const response = await fetch(`forms/${arquivo}`);
-    const html = await response.text();
-    formContainer.innerHTML = html;
+    renderFormElement(formNumber);
   });
 });
+
+renderFormElement(1);
